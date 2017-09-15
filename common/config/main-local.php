65db117da0c9e04e8270a866994c4aef4b46e8a0
s-nadesh <nadesh@arkinfotec.com>
2017-09-15 17:51:22 +0530

    initial commit

---
+++ common/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=sumanas_yee',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 3600,
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'yourname@gmail.com',
+                'password' => 'yourpassword',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+            'htmlLayout' => '@vendor/yeesoft/yii2-yee-auth/views/mail/layouts/html',
+            'textLayout' => '@vendor/yeesoft/yii2-yee-auth/views/mail/layouts/text',
+        ],
+    ],
+];
