35137a3cc6a5910f4d87f782dd100639c46efa89
s-nadesh <nadesh@arkinfotec.com>
2017-10-03 15:50:57 +0530

    Admin login layout

---
+++ frontend/views/layouts/login.php
@@ -0,0 +1,42 @@
+<?php
+/* @var $this View */
+/* @var $content string */
+
+use common\widgets\Alert;
+use frontend\assets\AppAsset;
+use frontend\assets\ThemeAsset;
+use yii\helpers\Html;
+use yii\web\View;
+
+Yii::$app->assetManager->forceCopy = true;
+AppAsset::register($this);
+ThemeAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <?= $this->renderMetaTags() ?>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-md-offset-3">
+                <?= Alert::widget() ?>
+                <?= $content ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
