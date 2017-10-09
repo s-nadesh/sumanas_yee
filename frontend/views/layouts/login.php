c1a509816bad5e4a60ff69aa6809f0785a1d50ad
s-nadesh <nadesh@arkinfotec.com>
2017-10-09 12:44:58 +0530

    Tracking codes placed in layouts

--- frontend/views/layouts/login.php
+++ frontend/views/layouts/login.php
@@ -15,28 +15,45 @@ ThemeAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <?= $this->renderMetaTags() ?>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
+    <head>
+        <!-- Google Tag Manager -->
+        <script>(function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({'gtm.start':
+                            new Date().getTime(), event: 'gtm.js'});
+                var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src =
+                        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-TZB9HBQ');</script>
+        <!-- End Google Tag Manager -->
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <?= $this->renderMetaTags() ?>
+        <?php $this->head() ?>
+    </head>
+    <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZB9HBQ"
+                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-5 col-md-offset-3">
-                <?= Alert::widget() ?>
-                <?= $content ?>
+        <div class="wrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5 col-md-offset-3">
+                        <?= Alert::widget() ?>
+                        <?= $content ?>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
 
-<?php $this->endBody() ?>
-</body>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
