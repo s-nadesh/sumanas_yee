f961a551254bfaa8085f7e4e8e6196c780540ac9
roobinidevi <31208856+roobinidevi@users.noreply.github.com>
2018-06-13 16:17:04 +0530

    #Change form validation link

--- frontend/assets/ThemeAsset.php
+++ frontend/assets/ThemeAsset.php
@@ -48,8 +48,8 @@ class ThemeAsset extends AssetBundle {
         'http://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js',
         'js/index.js',
         'js/slider.js',
-        'http://formvalidation.io/vendor/formvalidation/js/formValidation.min.js',
-        'http://formvalidation.io/vendor/formvalidation/js/framework/bootstrap.min.js',
+        'https://cdn.jsdelivr.net/jquery.formvalidation/0.6.1/js/formValidation.min.js',
+        'https://cdn.jsdelivr.net/jquery.formvalidation/0.6.1/js/framework/bootstrap.min.js',
         'js/reCaptcha2.min.js',
     ];
     public $depends = [
