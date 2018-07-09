5bd4200c8e51203860d82d851a1fa6ba5751dfcf
roobinidevi <31208856+roobinidevi@users.noreply.github.com>
2018-07-09 12:11:01 +0530

    Add scripts in body tag

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -109,6 +109,13 @@ ThemeAsset::register($this);
         "https://www.linkedin.com/company/25067058/",
         "https://twitter.com/sumanastech",                            
         "https://plus.google.com/+SumanasTech"]
+        },
+        {
+        "@context": "http://schema.org/",
+        "@type": "WebSite",
+        "name": "Sumanas Technologies",
+        "alternateName": "Sumanas Technologies",
+        "url": "http://www.sumanastech.com"
         }
     </script>
 
@@ -135,18 +142,18 @@ ThemeAsset::register($this);
     <div class="drawermenu-overlay"></div>
     <?php $this->endBody() ?>
     <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5a155482bb0c3f433d4ca99c/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function () {
+            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/5a155482bb0c3f433d4ca99c/default';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 </html>
 <?php $this->endPage() ?>
